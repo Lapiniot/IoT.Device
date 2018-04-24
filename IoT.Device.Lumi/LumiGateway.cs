676b443c0f38c1c34c16b948c378b99124d476e0
Lapiniot <aossss@gmail.com>
2018-04-24 13:52:15 +0300

    Lumi protocol separated

--- IoT.Device.Lumi/LumiGateway.cs
+++ IoT.Device.Lumi/LumiGateway.cs
@@ -3,34 +3,33 @@ using System.Collections.Generic;
 using System.Json;
 using System.Linq;
 using System.Net;
-using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using IoT.Device.Lumi.SubDevices;
-using IoT.Protocol.Udp;
-using CompletionDictionary = System.Collections.Concurrent.ConcurrentDictionary<string, System.Threading.Tasks.TaskCompletionSource<System.Json.JsonObject>>;
+using IoT.Protocol.Lumi;
+using IoT.Protocol.Lumi.Interfaces;
 using Cache = IoT.Device.ImplementationCache<
     IoT.Device.Lumi.SupportedSubDeviceAttribute,
     IoT.Device.Lumi.LumiSubDevice>;
 
 namespace IoT.Device.Lumi
 {
-    public sealed class LumiGateway : LumiThing
+    public sealed class LumiGateway : LumiThing, ILumiEventListener
     {
         private readonly Dictionary<string, LumiSubDevice> children;
-        private readonly CommandEndpoint client;
-        private readonly ListeningClient listener;
         private readonly SemaphoreSlim semaphore;
+        private LumiControlEndpoint client;
         private bool disposed;
         private int illumination;
+        private LumiEventListener listener;
         private int rgbValue;
 
         public LumiGateway(string address, ushort port, string sid) : base(sid)
         {
             semaphore = new SemaphoreSlim(1, 1);
             children = new Dictionary<string, LumiSubDevice>();
-            client = new CommandEndpoint(new IPEndPoint(IPAddress.Parse(address), port));
-            listener = new ListeningClient(this, new IPEndPoint(IPAddress.Parse("224.0.0.50"), port));
+            client = new LumiControlEndpoint(new IPEndPoint(IPAddress.Parse(address), port));
+            listener = new LumiEventListener(this, new IPEndPoint(IPAddress.Parse("224.0.0.50"), port));
         }
 
         public string Token { get; private set; }
@@ -64,6 +63,30 @@ namespace IoT.Device.Lumi
         public override string ModelName { get; } = "gateway";
 
         protected override TimeSpan OfflineTimeout { get; } = TimeSpan.FromSeconds(15);
+
+        public void OnReportMessage(string sid, JsonObject data, JsonObject message)
+        {
+            if(sid == Sid && message.TryGetValue("model", out var model) && model == "gateway")
+            {
+                UpdateState(data);
+            }
+            else if(children.TryGetValue(sid, out var device))
+            {
+                device.UpdateState(data);
+            }
+        }
+
+        public void OnHeartbeatMessage(string sid, JsonObject data, JsonObject message)
+        {
+            if(sid == Sid && message.TryGetValue("model", out var model) && model == "gateway")
+            {
+                Token = message["token"];
+            }
+            else if(children.TryGetValue(sid, out var device))
+            {
+                device.UpdateState(data);
+            }
+        }
 
         public void Connect()
         {
@@ -136,11 +159,6 @@ namespace IoT.Device.Lumi
             if(data.TryGetValue("illumination", out var i)) Illumination = i;
         }
 
-        private void Heartbeat(JsonObject data)
-        {
-            Token = data["token"];
-        }
-
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);
@@ -150,144 +168,17 @@ namespace IoT.Device.Lumi
                 if(disposing)
                 {
                     client.Dispose();
+                    client = null;
+
                     listener.Dispose();
+                    listener = null;
+
                     semaphore.Dispose();
 
                     foreach(var c in children) c.Value.Dispose();
                 }
 
                 disposed = true;
-            }
-        }
-
-        private class CommandEndpoint : UdpMessageDispatchingEndpoint<JsonObject, JsonObject>
-        {
-            private readonly CompletionDictionary completions = new CompletionDictionary();
-
-            public CommandEndpoint(IPEndPoint endpoint) : base(endpoint)
-            {
-            }
-
-            private TimeSpan CommandTimeout { get; } = TimeSpan.FromSeconds(10);
-
-            public override async Task<JsonObject> InvokeAsync(JsonObject message, CancellationToken cancellationToken)
-            {
-                var commandKey = GetCommandKey(message["cmd"], message["sid"]);
-
-                var completionSource = new TaskCompletionSource<JsonObject>();
-
-                try
-                {
-                    if(!completions.TryAdd(commandKey, completionSource))
-                    {
-                        throw new InvalidOperationException("Error scheduling new command completion task");
-                    }
-
-                    await SendDatagramAsync(message.Serialize(), cancellationToken).ConfigureAwait(false);
-
-                    using(var timeoutSource = new CancellationTokenSource(CommandTimeout))
-                    using(completionSource.Bind(cancellationToken, timeoutSource.Token))
-                    {
-                        return await completionSource.Task.ConfigureAwait(false);
-                    }
-                }
-                finally
-                {
-                    completions.TryRemove(commandKey, out _);
-                }
-            }
-
-            protected override UdpClient CreateUdpClient()
-            {
-                var client = new UdpClient {EnableBroadcast = false};
-
-                client.Connect(Endpoint);
-
-                return client;
-            }
-
-            protected override void OnDataAvailable(IPEndPoint remoteEndPoint, byte[] bytes)
-            {
-                var json = JsonExtensions.Deserialize(bytes);
-
-                if(json is JsonObject j && j.TryGetValue("cmd", out var cmd) && j.TryGetValue("sid", out var sid) &&
-                   completions.TryRemove(GetCommandKey(GetCmdName(cmd), sid), out var completionSource))
-                {
-                    completionSource.TrySetResult(j);
-                }
-            }
-
-            private string GetCommandKey(string command, string sid)
-            {
-                return $"{command}.{sid}";
-            }
-
-            private string GetCmdName(string command)
-            {
-                return command.EndsWith("_ack") ? command.Substring(0, command.Length - 4) : command;
-            }
-
-            public Task<JsonObject> InvokeAsync(string command, string sid, CancellationToken cancellationToken = default)
-            {
-                return InvokeAsync(new JsonObject {{"cmd", command}, {"sid", sid}}, cancellationToken);
-            }
-        }
-
-        private class ListeningClient : UdpListener
-        {
-            private readonly IPEndPoint endpoint;
-            private readonly LumiGateway gateway;
-
-            public ListeningClient(LumiGateway lumiGateway, IPEndPoint groupEndpoint) : base(groupEndpoint)
-            {
-                gateway = lumiGateway;
-                endpoint = groupEndpoint;
-            }
-
-            protected override UdpClient CreateUdpClient()
-            {
-                var client = new UdpClient(endpoint.Port);
-                client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-                client.JoinMulticastGroup(endpoint.Address, 64);
-                return client;
-            }
-
-            protected override void OnDataAvailable(IPEndPoint remoteEndPoint, byte[] bytes)
-            {
-                var json = (JsonObject) JsonExtensions.Deserialize(bytes);
-
-                if(json.TryGetValue("sid", out var sid) && json.TryGetValue("data", out var v) &&
-                   JsonValue.Parse(v) is JsonObject data)
-                {
-                    switch((string) json["cmd"])
-                    {
-                        case "report":
-                        {
-                            if(IsGateway())
-                            {
-                                gateway.UpdateState(data);
-                            }
-                            else if(gateway.children.TryGetValue(sid, out var device)) device.UpdateState(data);
-
-                            break;
-                        }
-                        case "heartbeat":
-                        {
-                            if(IsGateway())
-                            {
-                                gateway.Heartbeat(json);
-                            }
-                            else if(gateway.children.TryGetValue(sid, out var device)) device.UpdateState(data);
-
-                            break;
-                        }
-                    }
-
-                    bool IsGateway()
-                    {
-                        return json.TryGetValue("model", out var model) && model == "gateway" && sid == gateway.Sid;
-                    }
-                }
             }
         }
     }
