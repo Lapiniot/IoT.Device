a1c8a2b4adbfeace6f78554095f72755949fc0a0
Lapiniot <aossss@gmail.com>
2018-05-06 16:31:36 +0300

    code reformat

--- IoT.Device.Upnp/UpnpDevice.cs
+++ IoT.Device.Upnp/UpnpDevice.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 using IoT.Protocol.Upnp;
 
 namespace IoT.Device.Upnp
 {
     public class UpnpDevice
     {
-        public string Usn { get; }
         public UpnpDevice(Uri descriptionUri, string usn)
         {
             DescriptionUri = descriptionUri ?? throw new ArgumentNullException(nameof(descriptionUri));
 
             if(string.IsNullOrWhiteSpace(usn)) throw new ArgumentException("valid USN must be provided", nameof(usn));
-            
+
             Usn = usn;
         }
+
+        public string Usn { get; }
 
         public Uri DescriptionUri { get; }
 
         public Task<UpnpDeviceDescription> GetDescriptionAsync(CancellationToken cancellationToken = default)
         {
             return UpnpDeviceDescription.LoadAsync(DescriptionUri, cancellationToken);
-
         }
     }
 }
