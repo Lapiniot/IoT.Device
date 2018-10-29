6cb5b550ab00d23f87f4cba36a8c750205e0ec55
Lapiniot <aossss@gmail.com>
2018-10-29 14:50:11 +0200

    Aqara Water Leak sensor support

--- IoT.Device.Lumi/SubDevices/AqaraWaterLeakSensor.cs
+++ IoT.Device.Lumi/SubDevices/AqaraWaterLeakSensor.cs
@@ -1,4 +1,3 @@
-using System.Json;
 using IoT.Device.Metadata;
 using static IoT.Device.Metadata.Connectivity;
 using static IoT.Device.Metadata.PowerSource;
@@ -8,17 +7,12 @@ namespace IoT.Device.Lumi.SubDevices
     [ModelID("SJCGQ11LM")]
     [PowerSource(CR2032)]
     [Connectivity(ZigBee)]
-    public sealed class AqaraWaterLeakSensor : LumiSubDevice
+    public sealed class AqaraWaterLeakSensor : LumiSubDeviceWithStatus
     {
         private AqaraWaterLeakSensor(string sid, int id) : base(sid, id)
         {
         }
 
         public override string Model { get; } = "sensor_wleak.aq1";
-
-        protected internal override void OnStateChanged(JsonObject state)
-        {
-            base.OnStateChanged(state);
-        }
     }
 }
