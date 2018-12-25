1cb1f9c7094794c777b57f8149478e23f568c16d
Lapiniot <aossss@gmail.com>
2018-12-25 21:34:42 +0200

    minor code reformatting

--- IoT.Device.Xiaomi.Umi/UmiSpeakerEnumerator.cs
+++ IoT.Device.Xiaomi.Umi/UmiSpeakerEnumerator.cs
@@ -8,7 +8,7 @@ namespace IoT.Device.Xiaomi.Umi
     public class UmiSpeakerEnumerator : ConvertingEnumerator<SsdpReply, UmiSpeakerDevice>
     {
         public UmiSpeakerEnumerator() :
-            base(new SsdpEnumerator(searchTarget: "urn:schemas-upnp-org:device:UmiSystem:1"), new UpnpReplyComparer()) {}
+            base(new SsdpEnumerator("urn:schemas-upnp-org:device:UmiSystem:1"), new UpnpReplyComparer()) {}
 
         #region Overrides of ConvertingEnumerator<SsdpReply,UmiSpeakerDevice>
 
