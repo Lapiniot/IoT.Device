3017f6b668393c481d9c8266a7492d8753f0f470
Lapiniot <aossss@gmail.com>
2021-10-23 20:28:47 +0300

    DeviceFeature targets detection logic has been improved

--- IoT.Device.Generators/FilterSupportsFeatureAttributesSyntaxContextReceiver.cs
+++ IoT.Device.Generators/FilterSupportsFeatureAttributesSyntaxContextReceiver.cs
@@ -15,33 +15,39 @@ internal class FilterSupportsFeatureAttributesSyntaxContextReceiver : ISyntaxCon
             cd.Modifiers.Any(m => m.IsKind(PublicKeyword)) &&
             cd.Modifiers.Any(m => m.IsKind(PartialKeyword)) &&
             !cd.Modifiers.Any(m => m.IsKind(StaticKeyword)) &&
-            context.SemanticModel.GetDeclaredSymbol(context.Node) is ITypeSymbol ts)
+            context.SemanticModel.GetDeclaredSymbol(context.Node) is ITypeSymbol symbol)
         {
-            var attributes = ts.GetAttributes().Where(a => a is
-            {
-                AttributeClass:
-                {
-                    IsGenericType: true,
-                    BaseType:
-                    {
-                        Name: "SupportsFeatureAttribute",
-                        ContainingNamespace:
-                        {
-                            Name: "Device", ContainingNamespace:
-                            {
-                                Name: "IoT", ContainingNamespace.IsGlobalNamespace: true
-                            }
-                        }
-                    },
-                    TypeArguments.Length: 1
-                }
-            }).Select(a => a.AttributeClass!).ToArray();
+            var attributes = symbol.GetAttributes().Where(IsSupportsFeatureAtribute).Select(a => a.AttributeClass!).ToArray();
 
             if(attributes.Length != 0)
             {
-                Classes.Add((ts, attributes));
+                Classes.Add((symbol, attributes));
             }
         }
+    }
+
+    private static bool IsSupportsFeatureAtribute(AttributeData attribute)
+    {
+        return attribute is
+        {
+            AttributeClass:
+            {
+                IsGenericType: true,
+                BaseType:
+                {
+                    Name: "SupportsFeatureAttribute",
+                    ContainingAssembly.Name: "IoT.Device",
+                    ContainingNamespace:
+                    {
+                        Name: "Device", ContainingNamespace:
+                        {
+                            Name: "IoT", ContainingNamespace.IsGlobalNamespace: true
+                        }
+                    }
+                },
+                TypeArguments.Length: 1
+            }
+        };
     }
 
     public static IEnumerable<ITypeSymbol> EnumerateRelatedFeatureTypes(ITypeSymbol? featureType)
