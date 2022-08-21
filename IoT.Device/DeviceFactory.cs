8712724d383759c642607f102634bf6a0c0525bb
Lapiniot <aossss@gmail.com>
2022-08-21 23:36:54 +0300

    IoT.Device marked as trimmable

--- IoT.Device/DeviceFactory.cs
+++ IoT.Device/DeviceFactory.cs
@@ -1,5 +1,6 @@
 using System.Collections.Concurrent;
 using System.Diagnostics.CodeAnalysis;
+using static System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes;
 using static System.Reflection.BindingFlags;
 
 namespace IoT.Device;
@@ -10,9 +11,10 @@ public static class DeviceFactory<T>
     private static readonly ConcurrentDictionary<string, Type> Cache = new(StringComparer.Ordinal);
 #nullable enable
 
-    public static void Register<TImpl>(string model) where TImpl : T =>
+    public static void Register<[DynamicallyAccessedMembers(PublicConstructors | NonPublicConstructors)] TImpl>(string model) where TImpl : T =>
         Cache.AddOrUpdate(model, static (_, type) => type, static (_, _, type) => type, typeof(TImpl));
 
+    [UnconditionalSuppressMessage("AssemblyLoadTrimming", "IL2067:DynamicallyAccessedMembers", Justification = "Preserved by annotating Register<TImpl>.")]
     public static T? Create(string model, params object[] args) =>
         Cache.TryGetValue(model, out var type)
             ? (T?)Activator.CreateInstance(type, Public | NonPublic | Instance, null, args, null)
