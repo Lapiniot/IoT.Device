e3064fd1b2606b983e6d7059cae0ac972630da40
Lapiniot <aossss@gmail.com>
2025-07-25 21:31:19 +0300

    Make use of the new ArgumentParserGenerator version in order to demonstrate how CLI command specific options can be organized and selectively parsed

--- Samples/Yeelight.Control/Program.cs
+++ Samples/Yeelight.Control/Program.cs
@@ -1,15 +1,11 @@
 ﻿using OOs.CommandLine;
 using Yeelight.Control;
-[assembly: Command("discover", IsDefault = true), Command("exec"), Command("monitor")]
-[assembly: Option<string>("address", "address", 'a'), Option<string>("method", "method", 'm'), Option<string>("params", "params", 'p')]
-
-var arguments = Arguments.Parse(args, true);
 
 try
 {
-    switch (arguments.Command)
+    switch ((ReadOnlySpan<string>)args)
     {
-        case "discover":
+        case ["discover"] or []:
             using (var cts = new CancellationTokenSource())
             {
                 Console.WriteLine("Searching for Yeelight devices on the LAN...");
@@ -22,19 +18,21 @@ try
             }
 
             break;
-        case "exec":
+        case ["exec", .. var rest]:
             {
-                var address = GetArgument(arguments, "address", "Device address: ");
-                var method = GetArgument(arguments, "method", "Method: ");
-                var @params = GetArgument(arguments, "params", "Params: ");
+                var (options, _) = ExecCommandArgs.Parse(rest);
+                var address = GetOption(options, "address", "Device address: ");
+                var method = GetOption(options, "method", "Method: ");
+                var @params = GetOption(options, "params", "Params: ");
                 Console.WriteLine(await ExecCommand.RunAsync(address, method, @params).ConfigureAwait(false));
             }
 
             break;
-        case "monitor":
+        case ["monitor", .. var rest]:
             using (var cts = new CancellationTokenSource())
             {
-                var address = GetArgument(arguments, "address", "Device address: ");
+                var (options, _) = MonitorCommandArgs.Parse(rest);
+                var address = GetOption(options, "address", "Device address: ");
                 Console.WriteLine("Listening for device prop change notifications...");
                 Console.WriteLine("Press any key to exit.");
                 var runTask = MonitorCommand.RunAsync(address, new ReportPropChangeObserver(), cts.Token);
@@ -63,14 +61,22 @@ static async Task CancelOnUserInputAsync(CancellationTokenSource cts)
     await cts.CancelAsync().ConfigureAwait(false);
 }
 
-static string GetArgument(Arguments arguments, string name, string promptText)
+static string GetOption(IReadOnlyDictionary<string, string?> options, string name, string promptText)
 {
-    var argument = arguments.Options.TryGetValue(name, out var value) ? value as string : null;
-    while (string.IsNullOrEmpty(argument))
+    var option = options.TryGetValue(name, out var value) ? value as string : null;
+    while (string.IsNullOrEmpty(option))
     {
         Console.Write(promptText);
-        argument = Console.ReadLine();
+        option = Console.ReadLine();
     }
 
-    return argument;
+    return option;
 }
+
+[Option<string>("address", "address", 'a')]
+[Option<string>("method", "method", 'm')]
+[Option<string>("params", "params", 'p')]
+internal partial struct ExecCommandArgs { }
+
+[Option<string>("address", "address", 'a')]
+internal partial struct MonitorCommandArgs { }
