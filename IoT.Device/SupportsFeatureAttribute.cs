48b2b2813e5f99618278f5dd6d0c8a481b3240d3
Lapiniot <aossss@gmail.com>
2025-08-20 20:22:45 +0300

    Obsolete base abstract SupportsFeatureAttribute class has been removed from IoT.Device library

--- IoT.Device/SupportsFeatureAttribute.cs
+++
@@ -1,6 +0,0 @@
-namespace IoT.Device;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-public abstract class SupportsFeatureAttribute<TFeature, TFeatureImpl> : Attribute
-    where TFeatureImpl : TFeature
-{ }
